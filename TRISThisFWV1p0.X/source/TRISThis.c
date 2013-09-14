774da8245da0ea6ed47ad151893ac209eb8cf9b6
Matt Bennett <matt.bennett@gmail.com>
2013-09-14 16:46:45 -0500

    new file:   TRISThisFWV1p0.X/source/TRISThis.c 	modified:   TRISThisFWV1p0.X/include/LED.h 	modified:   TRISThisFWV1p0.X/include/TRISThis.h 	modified:   TRISThisFWV1p0.X/include/commsToRPi.h 	modified:   TRISThisFWV1p0.X/include/firmwareConfiguration.h 	modified:   TRISThisFWV1p0.X/include/hardwareConfiguration.h 	modified:   TRISThisFWV1p0.X/nbproject/Makefile-genesis.properties 	modified:   TRISThisFWV1p0.X/nbproject/configurations.xml 	modified:   TRISThisFWV1p0.X/source/LED.c 	modified:   TRISThisFWV1p0.X/source/commsToRPi.c 	modified:   TRISThisFWV1p0.X/source/mainv1p0.c

---
+++ TRISThisFWV1p0.X/source/TRISThis.c
@@ -0,0 +1,76 @@
+/******************************************************************************/
+/* File:   TRISThis.c                                                         */
+/* Author: matt                                                               */
+/*                                                                            */
+/* Created on September 14, 2013, 4:19 PM                                     */
+/******************************************************************************/
+
+// <editor-fold defaultstate="collapsed" desc="SLA">
+/******************************************************************************/
+/* Software License Agreement                                                 */
+/*                                                                            */
+/*                                                                            */
+/******************************************************************************/// </editor-fold>
+
+
+#include <common.h>
+#include <TRISThis.h>
+
+TRISTHIS_DATA_TYPE TRISThisData;
+
+void TRISThisDigitalConfigure(void)
+{
+    IO_OUT00=FALSE;
+    IO_OUT01=FALSE;
+    IO_OUT02=FALSE;
+    IO_OUT03=FALSE;
+    IO_OUT04=FALSE;
+    IO_OUT05=FALSE;
+    IO_OUT06=FALSE;
+    IO_OUT07=FALSE;
+    IO_OUT08=FALSE;
+    IO_OUT09=FALSE;
+    IO_OUT10=FALSE;
+    IO_OUT11=FALSE;
+    IO_OUT12=FALSE;
+    IO_OUT13=FALSE;
+    IO_OUT14=FALSE;
+    IO_OUT15=FALSE;
+    IO_DIRECTION00=TRIS_IN;
+    IO_DIRECTION01=TRIS_IN;
+    IO_DIRECTION02=TRIS_IN;
+    IO_DIRECTION03=TRIS_IN;
+    IO_DIRECTION04=TRIS_IN;
+    IO_DIRECTION05=TRIS_IN;
+    IO_DIRECTION06=TRIS_IN;
+    IO_DIRECTION07=TRIS_IN;
+    IO_DIRECTION08=TRIS_IN;
+    IO_DIRECTION09=TRIS_IN;
+    IO_DIRECTION10=TRIS_IN;
+    IO_DIRECTION11=TRIS_IN;
+    IO_DIRECTION12=TRIS_IN;
+    IO_DIRECTION13=TRIS_IN;
+    IO_DIRECTION14=TRIS_IN;
+    IO_DIRECTION15=TRIS_IN;
+    TRISThisReadDigitalInputs();
+    TRISThisReadDigitalLatches();
+    TRISThisReadDigitalDirection();
+}
+
+void TRISThisReadDigitalInputs(void)
+{
+    TRISThisData.digital[0].port.Val=(0xff&(PORTD>>1));
+    TRISThisData.digital[1].port.Val=(0xff&(PORTE));
+}
+
+void TRISThisReadDigitalLatches(void)
+{
+    TRISThisData.digital[0].latch.Val=(0xff&(LATD>>1));
+    TRISThisData.digital[1].latch.Val=(0xff&(LATE));
+}
+
+void TRISThisReadDigitalDirection(void)
+{
+    TRISThisData.digital[0].direction.Val=(0xff&(TRISD>>1));
+    TRISThisData.digital[1].direction.Val=(0xff&(TRISE));
+}
