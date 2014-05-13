79e6166e0301cc86969510383b1b185a9d9d71fe
matt.bennett@gmail.com <matt.bennett@gmail.com>
2014-05-12 19:55:32 -0500

    adding analog.c/.h
    	new file:   TRISThisFWV1p0.X/include/analog.h
    	new file:   TRISThisFWV1p0.X/source/analog.c

---
+++ TRISThisFWV1p0.X/include/analog.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   analog.h
+ * Author: mjb
+ *
+ * Created on May 12, 2014, 7:47 PM
+ */
+
+#ifndef ANALOG_H
+#define	ANALOG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ANALOG_H */
+
