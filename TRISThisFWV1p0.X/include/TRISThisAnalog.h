deb1fe1ff7a39ab0e76622ce326cbe51c686209b
matt.bennett@gmail.com <matt.bennett@gmail.com>
2014-05-12 20:33:47 -0500

    new file:   TRISThisFWV1p0.X/include/TRISThisAnalog.h 	new file:   TRISThisFWV1p0.X/include/TRISThisDigital.h 	deleted:    TRISThisFWV1p0.X/include/analog.h 	deleted:    TRISThisFWV1p0.X/include/desktop.ini 	renamed:    TRISThisFWV1p0.X/source/analog.c -> TRISThisFWV1p0.X/source/TRISThisAnalog.c 	new file:   TRISThisFWV1p0.X/source/TRISThisDigital.c

---
+++ TRISThisFWV1p0.X/include/TRISThisAnalog.h
@@ -0,0 +1,17 @@
+/******************************************************************************/
+/* File:   TRISThisAnalog.h                                                   */
+/* Author: Matt Bennett                                                       */
+/*                                                                            */
+/* Created on May 12, 2014, 8:22 PM                                           */
+/******************************************************************************/
+
+#ifndef _TRISTHIS_ANALOG_H_
+#define	_TRISTHIS_ANALOG_H_
+
+
+
+#else
+    #warning "Redundant include of TRISThisAnalog.h"
+
+#endif	/* _TRISTHIS_ANALOG_H_ */
+
---
+++ TRISThisFWV1p0.X/include/TRISThisDigital.h
@@ -0,0 +1,14 @@
+/******************************************************************************/
+/* File:   TRISThisDigital.h                                                  */
+/* Author: mjb                                                                */
+/*                                                                            */
+/* Created on May 12, 2014, 8:18 PM                                           */
+/******************************************************************************/
+
+#ifndef _TRISTHIS_DIGITAL_H_
+#define	_TRISTHIS_DIGITAL_H_
+
+#else
+    #warning "Redundant include of TRISThisDigital.h"
+#endif	/* _TRISTHIS_DIGITAL_H_ */
+
